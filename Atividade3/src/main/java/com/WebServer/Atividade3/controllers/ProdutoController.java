b80427d86961e539d7342694ae432fed78e256f6
Sr-Marcos <marcospegodesousa10@gmail.com>
2022-11-16 18:49:59 -0300

    COMPLETO

--- Atividade3/src/main/java/com/WebServer/Atividade3/controllers/ProdutoController.java
+++ Atividade3/src/main/java/com/WebServer/Atividade3/controllers/ProdutoController.java
@@ -1,8 +1,14 @@
 package com.WebServer.Atividade3.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +21,41 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	//valid em Produto produto javax.validation.Valid
-	@PostMapping
+	//valid em Produto produto javax.validation.Valid 
+	//@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT}) 
   public @ResponseBody Produto novoProduto( Produto produto) {
 	  produtoRepository.save(produto);
 	return produto;
   }
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findBynomeContainingIgnoreCase(parteNome);
+	//	return produtoRepository.searchByNameLike(parteNome);
+	}
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdPagina}")
+	public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdPagina){
+		if(qtdPagina>=5) qtdPagina = 5;
+		PageRequest page = PageRequest.of(numeroPagina, qtdPagina);
+		return produtoRepository.findAll(page);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProduto( @PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	//@Valid
+//	@PutMapping
+//	public Produto alterarProduto( Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
 }
--- Atividade3/src/main/java/com/WebServer/Atividade3/models/repositories/ProdutoRepository.java
+++ Atividade3/src/main/java/com/WebServer/Atividade3/models/repositories/ProdutoRepository.java
@@ -1,9 +1,26 @@
 package com.WebServer.Atividade3.models.repositories;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.WebServer.Atividade3.models.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-
+//public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
+	
+	public Iterable<Produto> findBynomeContainingIgnoreCase(String parteNome);
+	
+	
+//	findBynomeContaining
+//	findBynomeIsContaining
+//	findBynomeContains
+//	
+//	findBynomeStartsWith
+//	findBynomeEndWith
+//	
+//	findBynomeNotContaining
+	//exemplo com select cont linha42
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 }
